99ebfd587dfae9797a929e33da1ae52d06479d78
sieun <foryou@software4u.co.kr>
2023-11-10 23:24:08 +0900

    마이너 수정사항

--- js/common.js
+++ js/common.js
@@ -1,9 +1,27 @@
-function headerLoader() {
+function headerLoader(showLang=true) {
   return {
     content: '',
     loadContent: async function() {
       try {
-        let response = await fetch('./partials/header.html');
+        const filename = showLang ? "header.html" : "header-no-lang.html";
+        let response = await fetch(`./partials/${filename}`);
+        if (!response.ok) throw new Error('Failed to fetch content');
+        this.content = await response.text();
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  }
+}
+
+
+function personalNavLoader() {
+  return {
+    content: '',
+    loadContent: async function() {
+      try {
+        const filename = "personal-nav.html"
+        let response = await fetch(`./partials/${filename}`);
         if (!response.ok) throw new Error('Failed to fetch content');
         this.content = await response.text();
       } catch (error) {
